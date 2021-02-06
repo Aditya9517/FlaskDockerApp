d38638d68e718e9227cd584402c205ffe4f89ad4
Aditya Kalyan Jayanti <adityajayanti@Adityas-MacBook-Pro.local>
2021-02-06 13:06:47 -0800

    updated weather function

--- weather_info.py
+++ weather_info.py
@@ -1,5 +1,4 @@
 from os import environ
-
 from weatherbit.api import Api
 
 
@@ -7,7 +6,10 @@ class WeatherInfo:
     def __init__(self):
         self.api = Api(environ.get('WEATHER_BIT_API'))
 
-    def get_weather_information_city(self, city, state):
+    def get_weather_information_city(self, city, state, country='US'):
         self.api.set_granularity('daily')
-        forecast = self.api.get_forecast(city=city, state=state)
-        return forecast.get_series(['temp', 'precip'])
+        forecast = self.api.get_forecast(city=city, state=state, country=country)
+        forecast_data = forecast.get_series(['temp', 'precip'])
+        print(type(forecast_data))
+        return forecast_data[0]
+
